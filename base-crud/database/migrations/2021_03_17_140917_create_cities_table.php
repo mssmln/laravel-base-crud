2e80df26c9911c541f4e9d6f798c3471114958bc
Massimiliano <m.croci.ly@gmail.com>
2021-03-17 15:30:07 +0100

    migrated the table and added some entries

---
+++ base-crud/database/migrations/2021_03_17_140917_create_cities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',20);
+            $table->bigInteger('population');
+            $table->float('area',4,1);
+            $table->string('weather',50);
+            $table->string('area_code',10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
